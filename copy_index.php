72aba9ea426f0596dcff394a7af4fa09758f6739
WildRom <wild30973@gmail.com>
2020-06-20 23:47:38 +0100

    Created login and register forms

---
+++ copy_index.php
@@ -0,0 +1,54 @@
+<div class="col-lg-6 col-md-6 d-none d-md-block image-container">
+</div>
+
+
+<div class="col-lg-6 col-md-6 form-container">
+  <div class="col-lg-8 col-md-12 col-sm-9 col-xs-12 form-box text-center">
+    <div class="logo mb-3">
+      <img src="img/graphics-farm-283688.gif" alt="logo">
+    </div>
+    <div class="heading mb-4">
+      <h4 class="ltu">Prisijunkite</h4>
+      <h4 class="eng">Login into your account</h4>
+    </div>
+    <!-- forma -->
+     <form action="index.php" method="post">
+    <div class="form-input">
+      <span><i class="fa fa-envelope"></i></span>
+      <input type="email" name="email" placeholder="Email address..." required>
+    </div>>
+    <div class="form-input">
+      <span><i class="fa fa-lock"></i></span>
+      <input type="password" name="password" placeholder="Password..." required>
+    </div>
+    <div class="row mb-3">
+      <div class="col-6 d-flex">
+        <div class="custom-control custom-checkbox">
+          <input type="checkbox" class="custom-control-input" id="cb1" name="cb1">
+          <label for="cb1" class="cutom-control-label text-white ltu">Atsiminti</label>
+          <label for="cb1" class="cutom-control-label text-white eng">Remember</label>
+        </div>
+      </div>
+      <div class="col-6 text-right">
+        <a href="forget.php" class="forget-link ltu">Užmiršau slaptažodį</a>
+        <a href="forget.php" class="forget-link eng">Forget password</a>
+      </div>
+    </div>
+    <div class="text-left mb-3">
+      <button type="submit" class="btn ltu">Jungtis</button>
+      <button type="submit" class="btn eng">Login</button>
+    </div>
+    <div style="color: #777" class="ltu">Neturi akaunto?
+      <a href="register.php" class="register-link">Registruotis</a>
+    </div>
+    <div style="color: #777" class="eng">Don't have an account
+      <a href="register.php" class="register-link">Register here</a>
+    </div>
+  </form>
+
+    <!-- end  forma -->
+  </div>
+</div>
+
+
+ 
--- register.php
+++ register.php
@@ -12,7 +12,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
   <link rel="stylesheet" href="css/styles.css">
   <script src="https://code.jquery.com/jquery-3.5.1.slim.js"></script>
-  <title>Login</title>
+  <title>Register from</title>
 </head>
 
 <body>
@@ -20,19 +20,20 @@
     <div class="row">
       <div class="col-lg-6 col-md-6 d-none d-md-block image-container">
       </div>
-
-
       <div class="col-lg-6 col-md-6 form-container">
         <div class="col-lg-8 col-md-12 col-sm-9 col-xs-12 form-box text-center">
           <div class="logo mb-3">
             <img src="img/graphics-farm-283688.gif" alt="logo">
           </div>
           <div class="heading mb-4">
-            <h4 class="ltu">Prisijunkite</h4>
-            <h4 class="eng">Login into your account</h4>
+            <h4 class="ltu">Sukurkite akauntą</h4>
+            <h4 class="eng">Create an account</h4>
           </div>
-          <!-- forma -->
-          <form action="index.php" method="post">
+          <form action="register.php" method="post">
+            <div class="form-input">
+              <span><i class="fa fa-user"></i></span>
+              <input type="text" name="account_name" placeholder="Account Name..." required>
+            </div>
             <div class="form-input">
               <span><i class="fa fa-envelope"></i></span>
               <input type="email" name="email" placeholder="Email address..." required>
@@ -42,35 +43,31 @@
               <input type="password" name="password" placeholder="Password..." required>
             </div>
             <div class="row mb-3">
-              <div class="col-6 d-flex">
+              <div class="col-12 d-flex">
                 <div class="custom-control custom-checkbox">
                   <input type="checkbox" id="cb1" name="cb1">
-                  <label for="cb1" class="cutom-control-label text-white ltu">Atsiminti</label>
-                  <label for="cb1" class="cutom-control-label text-white eng">Remember</label>
+                  <label for="cb1" class="cutom-control-label text-white ltu">Sutinku su visomis sąlygomis</label>
+                  <label for="cb1" class="cutom-control-label text-white eng">I agree all terms & conditions</label>
                 </div>
-              </div>
-              <div class="col-6 text-right">
-                <a href="forget.php" class="forget-link ltu">Užmiršau slaptažodį</a>
-                <a href="forget.php" class="forget-link eng">Forget password</a>
               </div>
             </div>
             <div class="text-left mb-3">
-              <button type="submit" class="btn ltu">Jungtis</button>
-              <button type="submit" class="btn eng">Login</button>
+              <button type="submit" class="btn ltu">Registruotis</button>
+              <button type="submit" class="btn eng">Register</button>
             </div>
-            <div style="color: #777" class="ltu">Neturi akaunto?
-              <a href="register.php" class="register-link">Registruokis</a>
+            <div style="color: #777" class="ltu">Jau turi akauntą?
+              <a href="index.php" class="login-link">Junkis čia</a>
             </div>
-            <div style="color: #777" class="eng">Don't have an account
-              <a href="register.php" class="register-link">Register here</a>
-            </div>
-          </form>
-
-          <!-- end  forma -->
         </div>
+        <div style="color: #777" class="eng">Already have an account?
+          <a href="index.php" class="login-link">Login here</a>
+        </div>
+        </form>
       </div>
-    </div><!-- .row end -->
-  </div><!-- container -->
+    </div>
+
+  </div>
+  </div>
 
   <script src=" js/main.js"></script>
 </body>
